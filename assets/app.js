cdf732ab75a3379ad44b918cb3f11c1ad67c117c
Chris Fritz <chrisvfritz@gmail.com>
2016-08-24 20:09:25 -0400

    initial commit

---
+++ assets/app.js
@@ -0,0 +1,32 @@
+(function () { 'use strict'
+  var createApp = function () {
+    // ---------------------
+    // START NORMAL APP CODE
+    // ---------------------
+
+    // Main Vue instance must be returned and have a root
+    // node with the id "app", so that the client-side
+    // version can take over once it loads.
+    return new Vue({
+      template: '<div id="app">You have been here for {{ counter }} seconds.</div>',
+      data: {
+        counter: 0
+      },
+      created: function () {
+        var vm = this
+        setInterval(function () {
+          vm.counter += 1
+        }, 1000)
+      }
+    })
+
+    // ---------------------
+    // END NORMAL APP CODE
+    // ---------------------
+  }
+  if (typeof module !== 'undefined' && module.exports ) {
+    module.exports = createApp
+  } else {
+    this.app = createApp()
+  }
+}).call(this)
